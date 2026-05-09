4733df2373a980d648029cfbff31ca618d15471c
rahul p <rahulyts5@gmail.com>
2026-05-09 23:15:38 +0530

    remove fulltext index

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => array_filter(explode(',', env('ALLOWED_ORIGINS', 'http://localhost:5173'))),
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://vastrika-nx-frontend-git-main-rahulyts5s-projects.vercel.app',
+    ], 
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
