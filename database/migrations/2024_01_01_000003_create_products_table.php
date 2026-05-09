8f5eec9e96bc087f97e2d786079126d4cb703e17
rahul p <rahulyts5@gmail.com>
2026-05-09 22:22:21 +0530

    remove fulltext index

--- database/migrations/2024_01_01_000003_create_products_table.php
+++ database/migrations/2024_01_01_000003_create_products_table.php
@@ -34,7 +34,6 @@ return new class extends Migration
             $table->index('slug');
             $table->index('featured');
             $table->index('trending');
-            $table->fullText(['name', 'description']);
         });
     }
 
