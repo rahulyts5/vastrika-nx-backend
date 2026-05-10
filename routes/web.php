8d21df8b35e00511511017dc642bba12232a99cb
rahul p <rahulyts5@gmail.com>
2026-05-10 09:18:27 +0530

    cors error fixed

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/clear-config', function () {
+    Artisan::call('config:clear');
+    return 'Config cleared!';
+})->middleware('auth'); // remove middleware if needed
